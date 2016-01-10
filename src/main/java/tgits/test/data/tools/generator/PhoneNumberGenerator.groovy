fd1a9f1408db73f4d4ff356eaa508a404313e0ad
TGITS <christophe.vaudry@gmail.com>
2016-01-10 15:01:59 +0100

    Adding a method to generate a fixed size list of french phone numbers

--- src/main/java/tgits/test/data/tools/generator/PhoneNumberGenerator.groovy
+++ src/main/java/tgits/test/data/tools/generator/PhoneNumberGenerator.groovy
@@ -9,6 +9,25 @@ import tgits.test.data.tools.common.Randomizer
 class PhoneNumberGenerator {
     private Randomizer randomizer = Randomizer.instance
 
+    String getFrenchPhoneNumber() {
+        //Generate a phone number following this pattern "\\+\\d{11}|0\\d{9}"
+        StringBuilder number = new StringBuilder()
+
+        if (randomizer.nextBoolean()) {
+            number << "+"
+            number << randomizer.nextInt(10)
+            number << randomizer.nextInt(10)
+        } else {
+            number << "0"
+        }
+
+        for (int i = 0; i < 9; i++) {
+            number << randomizer.nextInt(10)
+        }
+
+        return number.toString()
+    }
+
     List<String> getListOfFrenchPhoneNumbers(int maxSizeList) throws IllegalArgumentException {
         if (maxSizeList <= 0) {
             throw new IllegalArgumentException("you must provide a value greater than 0")
@@ -27,22 +46,15 @@ class PhoneNumberGenerator {
         return list;
     }
 
-    String getFrenchPhoneNumber() {
-        //Generate a phone number following this pattern "\\+\\d{11}|0\\d{9}"
-        StringBuilder number = new StringBuilder()
-
-        if (randomizer.nextBoolean()) {
-            number << "+"
-            number << randomizer.nextInt(10)
-            number << randomizer.nextInt(10)
-        } else {
-            number << "0"
+    List<String> getFixedSizeListOfFrenchPhoneNumbers(int sizeList) throws IllegalArgumentException {
+        if (sizeList <= 0) {
+            throw new IllegalArgumentException("you must provide a value greater than 0")
         }
+        List<String> list = []
 
-        for (int i = 0; i < 9; i++) {
-            number << randomizer.nextInt(10)
+        (1..sizeList).each {
+            list << getFrenchPhoneNumber()
         }
-
-        return number.toString()
+        return list;
     }
 }
