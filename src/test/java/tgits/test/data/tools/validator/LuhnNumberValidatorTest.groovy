584562723480c7025d86fd5c024d0e01371bdbe2
TGITS <christophe.vaudry@gmail.com>
2016-01-10 16:23:19 +0100

    Transforming JUnit test in Spock test for LuhnNumberValidatorTest

--- src/test/java/tgits/test/data/tools/validator/LuhnNumberValidatorTest.groovy
+++ src/test/java/tgits/test/data/tools/validator/LuhnNumberValidatorTest.groovy
@@ -1,44 +1,63 @@
 package tgits.test.data.tools.validator
 
 import org.junit.Test
+import spock.lang.Shared
+import spock.lang.Specification
+import tgits.test.data.tools.common.Randomizer
 
-import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertFalse
-import static org.junit.Assert.assertTrue
 import static org.junit.Assert.assertTrue
 
 /**
  * Created by TGITS on 09/01/2016.
  */
-class LuhnNumberValidatorTest {
+class LuhnNumberValidatorTest extends Specification {
 
-    private static List<String> notNumbers = ["12345NotAStringThatIsANumber", "NotAStringThatIsANumber", "123.456"];
-    private static LuhnNumberValidator validator = LuhnNumberValidator.getInstance();
+    @Shared
+    LuhnNumberValidator validator
+    @Shared
+    String luhnNumber
+    @Shared
+    List<String> notLuhnNumbers
+    @Shared
+    Randomizer randomizer
 
-    @Test(expected=IllegalArgumentException.class)
-    public void testLuhnUtilsCheckWithNullArgument()
-    {
-        validator.isLuhnNumber(null);
+    def setupSpec() {
+        validator = LuhnNumberValidator.instance
+        randomizer = Randomizer.instance
     }
 
-    @Test(expected=IllegalArgumentException.class)
-    public void testLuhnUtilsCheckWithNotANumberArgument()
-    {
-        Random random = new Random();
-        int index = random.nextInt(notNumbers.size());
-        validator.isLuhnNumber(notNumbers[index]);
+    def setup() {
+        luhnNumber = null;
+        notLuhnNumbers = ["12345NotAStringThatIsANumber", "NotAStringThatIsANumber", "123.456"]
     }
 
-    @Test
-    public void testLuhnUtilsCheckWithNotLuhnNumberArgument()
-    {
-        assertFalse( validator.isLuhnNumber("927487086") );
+    def "check that 927487086 is not a Luhn number"() {
+        when:
+        luhnNumber = "927487086"
+        then:
+        !validator.isLuhnNumber(luhnNumber)
     }
 
-    @Test
-    public void testLuhnUtilsCheckWithLuhnNumberArgument()
-    {
-        assertTrue( validator.isLuhnNumber("972487086") );
+    def "check that 972487086 is a Luhn number"() {
+        when:
+        luhnNumber = "972487086"
+        then:
+        validator.isLuhnNumber(luhnNumber)
+    }
+
+    def "check that a null provided to the validator method will throw an IllegalArgumentException"() {
+        when:
+        validator.isLuhnNumber(null)
+        then:
+        thrown IllegalArgumentException
+    }
+
+    def "check that a value from the notLuhnNumbers list provided to the validator method will throw an IllegalArgumentException"() {
+        when:
+        validator.isLuhnNumber(notLuhnNumbers[randomizer.nextInt(notLuhnNumbers.size())]);
+        then:
+        thrown IllegalArgumentException
     }
 
 }
